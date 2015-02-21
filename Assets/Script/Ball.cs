4835099c80cf30e3821408e342f7103324bb0c1b
Tom Larrow <tomlarrow@gmail.com>
2015-02-21 17:46:37 -0500

    Added the ball.cs which right now has gravity off until the ball collides with the paddle for the first time.

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour 
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            rigidbody.useGravity = true;
+        }
+    }
+
+}
