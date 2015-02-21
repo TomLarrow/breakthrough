839da348f65849273b284dd4a135966769d9f8c5
Tom Larrow <tomlarrow@gmail.com>
2015-02-21 14:00:27 -0500

    Added the Paddle.cs script which moves the paddle to match that of the mouse.

---
+++ Assets/Script/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour 
+{
+
+    void FixedUpdate() 
+    {
+        // Set the position of the paddle to match the ScreenToWorldPoint of the mouse   
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        //Debug.Log(mousePos);
+        rigidbody.MovePosition(mousePos);
+	}
+}
