082c8fc17c93a912620559569832ffd3f1deaca7
Tom Larrow <tomlarrow@gmail.com>
2015-02-22 16:34:15 -0500

    4 bricks falling from the ceiling.  They are slow thanks to the drag,  Will have to figure this out a bit more.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour 
+{
+    public GameObject brick;
+    public float xPosition;
+    public float yPosition;
+    public int numBricks;
+    public int numColumns;
+    public float verticalDistance;
+    public float horizontalDistance;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        spawnBlockColumn(xPosition, yPosition, numBricks);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Spawn a row of blocks
+    void spawnBlockColumn (float xPos, float yPos, int numBlocks)
+    {
+        for (int i = 0; i < numBlocks; i++)
+        {
+            Instantiate(brick, new Vector3(xPos, yPos + (i * verticalDistance), 0), Quaternion.identity);
+        }
+    }
 }
