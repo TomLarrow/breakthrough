cfec9c521e03e551dc21e000e8734ebc72b72231
Tom Larrow <tomlarrow@gmail.com>
2015-02-22 20:34:31 -0500

    Added code to destroy the bricks.

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,9 +5,16 @@ public class Ball : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+        
+        // Start with Gravity off so that the ball just hovers there until the player hits it the first time.
         if (collision.collider.tag == "Player")
         {
             rigidbody.useGravity = true;
+        }
+
+        if (collision.collider.tag == "Brick")
+        {
+            Destroy(collision.collider.gameObject);
         }
     }
 
